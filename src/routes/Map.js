07b3545d050ab55e95b1121fc0e562cddf4e955e
MarlonX19 <marlon.englemam@gmail.com>
2019-06-09 19:57:43 -0300

    Fixed: getting the user's location correctly

--- src/routes/Map.js
+++ src/routes/Map.js
@@ -5,34 +5,35 @@ import MapView from 'react-native-maps';
 
 class Map extends Component {
 
-    state = {
-        region: null
-    }
+ state = {
+     region: null
+ }
 
     async componentDidMount() {
         navigator.geolocation.getCurrentPosition(
             ({ coords: { latitude, longitude } }) => {
+                console.log(latitude)
+                console.log(longitude)
+                console.log("succesfully got user's region")
                 this.setState({
-                    region: {
-                        latitude,
-                        longitude,
-                        latitudeDelta: 0.0143,
-                        longitudeDelta: 0.0134
-                    }
+                        region: {
+                            latitude: latitude,
+                            longitude: longitude,
+                            latitudeDelta: 0.0143,
+                            longitudeDelta: 0.0134
+                        }
                 })
             },
-            () => {},
+            () => { console.log("not able to get user's region")},
 
             {
                 timeout: 5000,
-                enableHighAccuracy: true,
-                maximumAge: 1000
+                enableHighAccuracy: true
             }
         )
     }
 
     render() {
-
         const { region } = this.state;
 
         return (
