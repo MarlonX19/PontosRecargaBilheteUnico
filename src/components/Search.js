cd1f0c3185417cd4dd2ed51fe00caa001a5197c3
MarlonX19 <marlon.englemam@gmail.com>
2019-06-10 00:17:05 -0300

    Visual improvements

--- src/components/Search.js
+++ src/components/Search.js
@@ -26,6 +26,54 @@ class Search extends Component {
                         top: Platform.select({ ios: 60, android: 30 }),
                         width: '100%'
                     },
+                    textInputContainer: {
+                        flex: 1,
+                        backgroundColor: 'transparent',
+                        height: 54,
+                        marginHorizontal: 20,
+                        borderTopWidth: 0,
+                        borderBottomWidth: 0,
+                    },
+                    textInput: {
+                        height: 54,
+                        margin: 0,
+                        borderRadius: 0,
+                        paddingTop: 0,
+                        paddingBottom: 0,
+                        paddingLeft: 15,
+                        paddingRight: 15,
+                        marginTop: 0,
+                        marginBottom: 0,
+                        marginLeft: 0,
+                        marginRight: 0,
+                        elevation: 5,
+                        shadowColor: "#000",
+                        shadowOpacity: 0.1,
+                        shadowOffset: {x: 0, y: 0},
+                        shadowRadius: 15,
+                        borderWidth: 1,
+                        borderColor: "#DDD",
+                        fontSize: 18
+                    },
+                    listView: {
+                        borderWidth: 1,
+                        borderColor: "#DDD",
+                        backgroundColor: "#FFF",
+                        marginHorizontal: 20,
+                        elevation: 5,
+                        shadowColor: "#000",
+                        shadowOpacity: 0.1,
+                        shadowOffset: {x: 0, y: 0},
+                        shadowRadius: 10,
+                        marginTop: 5
+                    },
+                    description: {
+                        fontSize: 14
+                    },
+                    row: {
+                        padding: 10,
+                        height: 50
+                    }
                   
                 }}
            />
