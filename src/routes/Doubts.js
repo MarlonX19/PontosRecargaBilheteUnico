f178a7ad00f83424c269586a78c1addb7c099a3f
MarlonX19 <marlon.englemam@gmail.com>
2019-06-15 17:20:54 -0300

    Added doubts section

--- src/routes/Doubts.js
+++ src/routes/Doubts.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, SectionList } from 'react-native';
+
+import MenuButton from '../components/MenuButton';
 
 class Doubts extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            data: [
+                { title: 'Title1', data: ['item1', 'item2'] },
+                { title: 'Title2', data: ['item3', 'item4'] },
+                { title: 'Title3', data: ['item5', 'item6'] },
+            ]
+        }
+
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>MyClass</Text>
+                <View style={{ flex: 1 }}>
+                    <MenuButton navigation={this.props.navigation} />
+                </View>
+
+                <View style={{ flex: 9, alignItems: 'flex-start', justifyContent: 'flex-start', backgroundColor: '#483d8b', paddingTop: 15 }}>
+                <SectionList
+                   sections={this.state.data}
+                   renderSectionHeader={( {section} ) => <Text style={styles.title} >{section.title}</Text> }
+                   renderItem={( {item} ) => <Text style={styles.item}>{item}</Text> }
+                   keyExtractor={( item, index ) => index }
+                   />
+                </View>
             </View>
         );
     }
@@ -14,10 +40,19 @@ class Doubts extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
     },
+
+    title: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        marginLeft: 5
+    },
+
+    item: {
+        fontSize: 18,
+        fontWeight: '400',
+        marginLeft: 10
+    }
 });
 
 export default Doubts;
