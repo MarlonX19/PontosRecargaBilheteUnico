5ce6f75ec2516052838e536ef22afff7cdd0a108
MarlonX19 <marlon.englemam@gmail.com>
2019-09-05 00:29:15 -0300

    Improvements

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Button, Image, Text, Dimensions, StatusBar, ImageBackground} from 'react-native';
+import { View, Button, Image, Text, Dimensions, StatusBar, ImageBackground, StyleSheet } from 'react-native';
 
 import MenuButton from '../components/MenuButton';
 
@@ -9,7 +9,7 @@ export default class Home extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1 }}>
+      <View style={styles.container}>
         <StatusBar
           barStyle="dark-content"
           backgroundColor="#e3f1f1"
@@ -17,32 +17,46 @@ export default class Home extends React.Component {
         <View style={{ flex: 1 }}>
           <MenuButton navigation={this.props.navigation} screenTitle='Pontos de recarga Bilhete Único' />
         </View>
-        <View style={{ flex: 9, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
-          <View style={{ flex: 2, backgroundColor: '#A9DAD6', borderRadius: 5, elevation: 3, justifyContent: 'center', alignItems: 'center', marginHorizontal: 10, marginVertical: 5 }}>
-            <Text style={{ fontSize: 20, color: 'black', padding: 20 }}>
-              Veja no mapa os pontos comerciais que efetuam recarga para Bilhete Único SPTrans
-            </Text>
+
+        <View style={styles.mainCards}>
+          <View style={{ flexDirection: 'column', flex: 1 }}>
+            <Text style={{ fontSize: 18, fontWeight: 'bold', color:"#A9DAD6" }}>Adquira crédito</Text>
+            <Text>Compre crédito para o sistema de transporte público de São Paulo</Text>
           </View>
-          <View style={{ flex: 2, backgroundColor: '#e3f1f1', borderRadius: 5, elevation: 3, justifyContent: 'center', alignItems: 'center', marginHorizontal: 10, marginVertical: 5 }}>
-            <Text style={{ fontSize: 14, color: 'black', padding: 20 }}>
-              Encontre o ponto de recarga de bilhete único mais próximo de você!
-              São centenas de estabelecimentos comercias espalhadas pela cidade que prestam o
-              serviço de recarga!
-            </Text>
-          </View>
-          <View style={{ flex: 5, justifyContent: 'center', alignItems: 'center' }}>
-            <Image style={{ height: 160, width: IMGWIDTH * 0.8, borderRadius: 5 }} source={require('../assets/imgs/SPTransCardHome.jpg')} />
-          </View>
-          <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center' }}>
-            <Button
-              onPress={() => this.props.navigation.toggleDrawer()}
-              title="Explorar o app"
-              color="green"
-              accessibilityLabel="See the map"
-            />
-          </View>
+          <Image style={{ width: 100, height: 80 }} source={require('../assets/imgs/cardIcon.png')} />
         </View>
+
+        <View style={styles.mainCards}>
+          <View style={{ flexDirection: 'column', flex: 1 }}>
+            <Text style={{ fontSize: 18, fontWeight: 'bold', color:"#A9DAD6" }}>Onde adquirir</Text>
+            <Text>Há centenas de estabelecimentos espalhados pela cidade onde é possível carregar seu Bilhete único nas diversas modalidades</Text>
+          </View>
+          <Image style={{ width: 80, height: 80 }} source={require('../assets/imgs/storeIcon.png')} />
+        </View>
+
+        <View style={styles.mainCards}>
+          <View style={{ flexDirection: 'column', flex: 1 }}>
+            <Text style={{ fontSize: 18, fontWeight: 'bold', color:"#A9DAD6" }}>Use seu crédito</Text>
+            <Text>Após carregar seu bilhete único corretamente basta usá-lo em ônibus, trem, metrô ou CPTM</Text>
+          </View>
+          <Image style={{ width: 80, height: 80 }} source={require('../assets/imgs/busIcon.png')} />
+        </View>
+
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  mainCards: {
+    flex: 3,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10
+  }
+
+});
