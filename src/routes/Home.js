ac0062966678d3a94e6b672514a95b955d7a994b
MarlonX19 <marlon.englemam@gmail.com>
2019-09-05 01:05:50 -0300

    Fix

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -20,26 +20,30 @@ export default class Home extends React.Component {
 
         <View style={styles.mainCards}>
           <View style={{ flexDirection: 'column', flex: 1 }}>
-            <Text style={{ fontSize: 18, fontWeight: 'bold', color:"#A9DAD6" }}>Adquira crédito</Text>
-            <Text>Compre crédito para o sistema de transporte público de São Paulo</Text>
+            <Text style={styles.title}>Adquira crédito</Text>
+            <Text style={styles.description}>Compre crédito para o sistema de transporte público de São Paulo</Text>
           </View>
           <Image style={{ width: 100, height: 80 }} source={require('../assets/imgs/cardIcon.png')} />
         </View>
 
         <View style={styles.mainCards}>
           <View style={{ flexDirection: 'column', flex: 1 }}>
-            <Text style={{ fontSize: 18, fontWeight: 'bold', color:"#A9DAD6" }}>Onde adquirir</Text>
-            <Text>Há centenas de estabelecimentos espalhados pela cidade onde é possível carregar seu Bilhete único nas diversas modalidades</Text>
+            <Text style={styles.title}>Onde adquirir</Text>
+            <Text style={styles.description}>Há centenas de estabelecimentos espalhados pela cidade onde é possível carregar seu Bilhete Único nas diversas modalidades</Text>
           </View>
           <Image style={{ width: 80, height: 80 }} source={require('../assets/imgs/storeIcon.png')} />
         </View>
 
         <View style={styles.mainCards}>
           <View style={{ flexDirection: 'column', flex: 1 }}>
-            <Text style={{ fontSize: 18, fontWeight: 'bold', color:"#A9DAD6" }}>Use seu crédito</Text>
-            <Text>Após carregar seu bilhete único corretamente basta usá-lo em ônibus, trem, metrô ou CPTM</Text>
+            <Text style={styles.title}>Use seu crédito</Text>
+            <Text style={styles.description}>Após carregar seu bilhete único corretamente basta usá-lo em ônibus, trem, metrô ou CPTM</Text>
           </View>
           <Image style={{ width: 80, height: 80 }} source={require('../assets/imgs/busIcon.png')} />
+        </View>
+
+        <View style={{ flex: 2, justifyContent: 'flex-end', alignItems: 'center' }}>
+          <Text style={{ color: 'blue' }}>Termos de uso</Text>
         </View>
 
       </View>
@@ -56,7 +60,22 @@ const styles = StyleSheet.create({
     flex: 3,
     flexDirection: 'row',
     justifyContent: 'space-around',
-    padding: 10
+    alignItems: 'center',
+    padding: 10,
+    margin: 5,
+    backgroundColor: '#EFEFEF',
+    borderRadius: 10
+  },
+
+  title: {
+    fontFamily: 'sans-serif-thin',
+    fontSize: 18,
+    fontWeight: 'bold',
+    color:"#A9DAD6"
+  },
+
+  description: {
+    fontFamily: 'sans-serif-light'
   }
 
 });
